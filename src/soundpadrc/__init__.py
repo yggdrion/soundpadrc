97ed9c01d7dd9d95dc96703c4fea98a7c923bf4b
r4b2 <github@r4b2.de>
2025-02-02 11:42:35 +0100

    feat: test version

--- src/soundpadrc/__init__.py
+++ src/soundpadrc/__init__.py
@@ -10,7 +10,6 @@ A basic wrapper for controlling Soundpad remotely.
 __title__ = "soundpadrc"
 __author__ = "r4b2"
 __license__ = "MIT"
-# __version__ = "0.3.0"
 
 from .soundpadrc import Soundpad
 
